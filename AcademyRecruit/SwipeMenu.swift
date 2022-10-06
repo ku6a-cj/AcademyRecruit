ad6bc9a269bbff8c246af333cecebd56d9ec5e02
CJ <kubachod@gmail.com>
2022-10-06 22:32:31 +0200

    bugs fixed

---
+++ AcademyRecruit/SwipeMenu.swift
@@ -0,0 +1,20 @@
+//
+//  SwipeMenu.swift
+//  AcademyRecruit
+//
+//  Created by Jakub Chodara on 06/10/2022.
+//
+
+import SwiftUI
+
+struct SwipeMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwipeMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        SwipeMenu()
+    }
+}
