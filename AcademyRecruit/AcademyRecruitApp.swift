e6679e2c3845031f9e4e973585904ca5adc1e43d
CJ <kubachod@gmail.com>
2022-10-06 19:42:52 +0200

    Initial Commit

---
+++ AcademyRecruit/AcademyRecruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyRecruitApp.swift
+//  AcademyRecruit
+//
+//  Created by Jakub Chodara on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AcademyRecruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
